e9ebb1f9eed97d4c169eb9bd52ed281a34af2a9b
Nicolas DIGARD <radicaldingos@gmail.com>
2016-05-18 22:22:52 +0200

    Cleaned unused files.

--- views/item/_form.php
+++
@@ -1,9 +0,0 @@
-<div class="item-form">
-
-    <?= $form->field($node, 'reference')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($node, 'status')->textInput() ?>
-
-    <?= $form->field($node, 'priority')->textInput() ?>
-
-</div>
