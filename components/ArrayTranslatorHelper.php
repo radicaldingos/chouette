c1788f7826115fc6b3b7097db29963b3503c1c75
Nicolas DIGARD <radicaldingos@gmail.com>
2016-05-20 17:38:00 +0200

    Added some phpdoc.

--- components/ArrayTranslatorHelper.php
+++ components/ArrayTranslatorHelper.php
@@ -5,8 +5,19 @@ namespace app\components;
 use Yii;
 use yii\helpers\BaseArrayHelper;
 
+/**
+ * Class ArrayTranslatorHelper
+ * 
+ * Allows the same things than BaseArrayHelper, with integrated translation of
+ * values in arrays.
+ */
 class ArrayTranslatorHelper extends BaseArrayHelper
 {
+    /**
+     * @inheritDoc
+     * 
+     * Values are translated by Yii::t().
+     */
     public static function map($array, $from, $to, $group = null)
     {
         $result = [];
