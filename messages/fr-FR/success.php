2b7d354e6273f0ebeadcdf8693faa07f96e70b75
Nicolas DIGARD <radicaldingos@gmail.com>
2016-05-24 19:00:25 +0200

    Added section name in messages.

--- messages/fr-FR/success.php
+++ messages/fr-FR/success.php
@@ -3,4 +3,6 @@
 return [
     'Requirement <b>{name}</b> has been created.' => "L'exigence <b>{name}</b> a été créée.",
     'Requirement <b>{name}</b> has been updated.' => "L'exigence <b>{name}</b> a été mise à jour.",
+    'Section <b>{name}</b> has been created.' => "La section <b>{name}</b> a été créée.",
+    'Section <b>{name}</b> has been updated.' => "La section <b>{name}</b> a été mise à jour.",
 ];
